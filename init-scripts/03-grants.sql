0ee3e11cdb8a0ad8c7ec3f3f52afd43e87e179c6
RafaelClaumann <rafael_claumann@hotmail.com>
2025-08-13 20:22:57 -0300

    criando arquivo de grants.sql
    arquivo contendo roles e grants do banco de dados

---
+++ init-scripts/03-grants.sql
@@ -0,0 +1,15 @@
+CREATE ROLE 'pdv_role';
+GRANT SELECT, INSERT, UPDATE, DELETE ON loja.Clientes TO 'pdv_role';
+GRANT SELECT, INSERT, UPDATE, DELETE ON loja.Produtos TO 'pdv_role'; 
+GRANT SELECT, INSERT, UPDATE, DELETE ON loja.Pedidos TO 'pdv_role'; 
+GRANT SELECT, INSERT, UPDATE, DELETE ON loja.ItensPedidos TO 'pdv_role';
+GRANT SELECT, INSERT, UPDATE, DELETE ON loja.Enderecos TO 'pdv_role';
+GRANT SELECT ON loja.Funcionarios TO 'pdv_role';
+GRANT SELECT ON loja.Status TO 'pdv_role';
+
+CREATE USER 'app_pdv'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT 'pdv_role' TO 'app_pdv'@'%';
+SET DEFAULT ROLE 'pdv_role' TO 'app_pdv'@'%';
+
+-- SHOW GRANTS FOR 'pdv_role';
+-- SHOW GRANTS FOR 'app_pdv'@'%';
