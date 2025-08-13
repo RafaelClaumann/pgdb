9bba2aa7ce7833b1e5b798958318cabc5897a6c7
RafaelClaumann <rafael_claumann@hotmail.com>
2025-08-13 20:21:44 -0300

    criando arquivo root.sql
    esse arquivo foi criado para que conseguissemos definir ROLES e GRANTS no phpmyadmin

---
+++ init-scripts/00-root.sql
@@ -0,0 +1,5 @@
+DROP USER IF EXISTS 'root'@'%';
+CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
