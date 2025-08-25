7c06bfe3c8354589ac0ac45b13ab126eb4ba0a52
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-08-25 20:11:31 -0300

    permissoes e schema desafio02

---
+++ desafio02/init-scripts/00-root.sql
@@ -0,0 +1,5 @@
+DROP USER IF EXISTS 'root'@'%';
+CREATE USER 'root'@'%' IDENTIFIED WITH caching_sha2_password BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
